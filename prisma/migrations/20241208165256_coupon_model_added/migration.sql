bfc0222098d1a249caf54d4047710688c8c0e988
shakib2123 <mshakibalhasan21@gmail.com>
2024-12-09 11:48:24 +0600

    coupon model created

---
+++ prisma/migrations/20241208165256_coupon_model_added/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "coupons" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdByRole" TEXT NOT NULL,
+    "createdById" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "coupons_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "coupons_code_key" ON "coupons"("code");
